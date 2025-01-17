bbf299b0bb178b1e765eae480235038e26423bcc
ifancy803 <2354587894@qq.com>
2025-01-18 00:28:38 +0800

    ui

---
+++ Assets/Scripts/UI/gostaff.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class gostaff : MonoBehaviour
+{
+    
+    
+        public void GOstaff()
+        {
+            SceneManager.LoadScene("StaffScene");
+        }
+    
+}
